53309949b9f34074d5aebe0cf4ce142adf2e704e
Erlangga Indra Permana <erlangga.ip@gmail.com>
2016-12-22 09:03:23 +0700

    update Dummy

--- Dummy.java
+++ Dummy.java
@@ -4,5 +4,13 @@ public class Dummy {
 		for(int i=1000;i>=12;i-=10){
 			System.out.println("Ini adalah nilai ke: "+i);
 		}
+		
+		int tes     = 100;
+		for (int i = 100; i >= 0; i--){
+			tes+=i;
+			System.out.println("Nilai tes: " +      tes);
+		}
+		
+		System.out.print(tes == 5150);
 	}
 }
